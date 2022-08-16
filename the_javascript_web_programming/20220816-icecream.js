0a458485c10e965b8ebb7f4dec278d1eee4d392a
Seunghyun Kim <kshyun.dev@gmail.com>
2022-08-16 18:05:16 +0900

    vs

---
+++ the_javascript_web_programming/20220816-icecream.js
@@ -0,0 +1,6 @@
+let members = 29;
+let price = 1000;
+let gogup = 2000;
+let result = members * price + gogup;
+
+console.log(result);
---
+++ the_javascript_web_programming/20220816-icecream.py
@@ -0,0 +1,4 @@
+members = 29
+price = 1000
+result = members * price
+print(result)
