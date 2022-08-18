7a420e787d94816ad5e12fd521cd7ba704929460
Seunghyun Kim <kshyun.dev@gmail.com>
2022-08-18 18:02:29 +0900

    vs

--- the_javascript_web_programming/20220816-change.js
+++ the_javascript_web_programming/20220816-change.js
@@ -1,16 +1,21 @@
 let a = 1;
 let b = 2; 
-let memory
 
 // 요구사항: a와 b의 값을 서로 바꾸시오
 
-// 메모
+// 메모 : 
 // 값을 불러오는 개념이 핵심이라고 한다
 // 그냥 불러오면 어느 하나가 먼저 불러와서 서로 바뀌지 않고 똑같이 될 거 같다
 // 값을 저장하면 되지 않을까?
 
-memory = b;
+let memory = b;
 b = a;
-a = temp;
+a = memory;
+
+console.log(a); 
+console.log(b); 
+
+
+
 
 
---
+++ the_javascript_web_programming/20220818-function-2.js
@@ -0,0 +1,9 @@
+function insa(parameter) {
+  parameter * 1;
+  return "두진";
+}
+
+// 데이터타입 : string -> 리턴 때문이다
+// console.log(typeof(insa("형민")));
+
+console.log(insa("은호") === "두진");
