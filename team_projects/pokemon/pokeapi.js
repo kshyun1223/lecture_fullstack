692921bb00217e3fdde1e7d097c85f4fb30fa49e
Seunghyun Kim <kaygon@naver.com>
2022-09-04 10:11:40 +0900

    update pokeapi.js

--- team_projects/pokemon/pokeapi.js
+++ team_projects/pokemon/pokeapi.js
@@ -14,7 +14,9 @@ const pokeAPI = function(number){
       let abilityOne = _POKEMON_API.abilities[0].ability.name;
       let abilityTwo = _POKEMON_API.abilities[1].ability.name; 
       root.innerHTML = `
-        <ul>
+        <div></div>
+        <div></div>
+        <div></div>
           <li>이름: ${name}</li>
           <br>
           <li>앞모습</li>
@@ -35,3 +37,19 @@ const pokeAPI = function(number){
     });
 }
 pokeAPI(1);
+
+card = document.createElement('div
+title = document.createElement('div'); 
+text = document.createElement('div'); 
+
+domStyling.size(card, box.clientWidth/5 + "px", box.clientHeight/2 + "px"); 
+card.style.backgroundColor = domInit.colorList[i]; 
+domStyling.text(card, domInit.slideNumber[i]); 
+
+domStyling.text(title, domInit.slideItem[i]); 
+domStyling.size(title, box.clientWidth/5 + "px", box.clientHeight/10 + "px
+domStyling.text(title, domInit.slideItem[i]); 
+
+domStyling.size(text, box.clientWidth/5 + "px", box.clientHeight/4 + "px"); 
+domStyling.text(text, domInit.text); 
+text.style.fontSize = "0.8rem";
