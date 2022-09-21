6a53c093e4d24f8bca44c3f2c9c56de82e95d4aa
seunghyun kim <kshyun.dev@gmail.com>
2022-09-21 12:39:50 +0900

    vs

--- lecture_practice-react/20220915-process-stdin.js
+++ lecture_practice-react/20220915-process-stdin.js
@@ -1,4 +1,4 @@
-// console.log('work test'); // 파일만들때 습관처럼 하는 테스트입니다.
+console.log('work test'); // 파일만들때 습관처럼 하는 테스트입니다.
 
 // unicode set, 해당 메서드를 선언하지 않으면 버퍼(buffer) 형식의 데이터를 받습니다.
 process.stdin.setEncoding('utf8');
@@ -11,5 +11,3 @@ process.stdin.on('data', function(data) {
   console.timeEnd('monitor'); // 종료직전 걸린 시간을 콘솔에 기록을 남겨줍니다.
   process.exit();  // 입력이벤트를 종료합니다.
 });
-
-//*외않되...ㅠㅠ
