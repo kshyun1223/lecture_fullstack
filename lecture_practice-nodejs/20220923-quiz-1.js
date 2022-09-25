7c82c46f7d96252a041d71e1468155ed6e67bc97
Seunghyun Kim <kshyun.dev@gmail.com>
2022-09-25 15:02:03 +0900

    vs

--- lecture_practice-nodejs/20220923-quiz-1.js
+++ lecture_practice-nodejs/20220923-quiz-1.js
@@ -9,9 +9,9 @@ const setdomComponent = (tagName, content, attributeName, props) => {
       if(getDataArr.length === 0) { // 내용이 없으면
         let tempArray = []; // virtualDOM...?
         tempArray.push("<"); // 꺽쇠 넣고
-        tempArray.push(tagName + '\u00A0' + attributeName +"=\""); // 태그명 띄고 속성명 이퀄 | \u00A0 === &nbsp
+        tempArray.push(tagName + attributeName +"=\"");
         for(const keys in props) { // props 매개변수로 들어온 객체에서 키만 순회하면서
-          tempArray.push( '\u00A0' + keys + ":" + props[keys] + ";"); // 한칸띄고 키 : 그 키의 값;
+          tempArray.push( keys + ":" + props[keys] + ";"); // 한칸띄고 키 : 그 키의 값;
         };
         tempArray.push("\">");
         getDataArr.push(tempArray.join(''));
@@ -52,10 +52,4 @@ http.createServer((request, response) => {
 }).listen(5555, () => {
   console.log(byteChecker(mainPage), "server listening on port 5555");
 });
-
-/* 문제점이 뭘까? */
-// 1. 내용을 수정하고 다시 실행해보면 이미 포트가 사용중이라고 에러가 뜬다
-// 2. fs 변수를 만들어 놨는데 관련 내용은 없다
-
-
 
