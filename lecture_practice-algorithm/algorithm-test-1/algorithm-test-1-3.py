cbee20ba9b6688a312f2ca6187c8777362a2dec4
seunghyun kim <kshyun.dev@gmail.com>
2022-09-26 16:14:02 +0900

    vs

---
+++ lecture_practice-algorithm/algorithm-test-1/algorithm-test-1-3.py
@@ -0,0 +1,29 @@
+string = "We will never be able to recognize opportunities if we first don’t recognize all that we have and all that we’ve been given."
+
+def apostrophe():
+    count=0
+    while count<string.length:
+    for ’ in string:
+      count = count+1
+      if ’
+    # for(let loop = 0; loop < string.length; loop++) {
+    # if(string[loop] === \’) {
+    #   count++;
+    # }
+    #   }
+    #   return count
+    # }
+    # apostrophe()
+
+# let words = function(){
+#   let countOfWords = 0;
+#   let wordsArr = string.trim().split(' '); 
+#   for (let i = 0; i < wordsArr.length; i++) {
+#     if (wordsArr[i] !== '') {
+#       countOfWords++;
+#     }
+#   }
+#   // console.log(countOfWords)
+#   return countOfWords
+# }
+# console.log(apostrophe() + words());
