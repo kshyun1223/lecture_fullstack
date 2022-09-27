e45a72f11b7c9ee54e47a108c0db6dd7ca80f264
seunghyun kim <kshyun.dev@gmail.com>
2022-09-27 16:28:24 +0900

    vs

---
+++ lecture_practice-nodejs/20220927-login-worker.js
@@ -0,0 +1,18 @@
+const element = {
+  header: "<header> this is header </header>",
+  body: "<body> this is body </body>",
+  footer: "<footer> this is footer </footer>",
+  form: `
+    <form id="log-in-form" method="GET">
+      <input type="text" name="username" placeholder="username" />
+      <input type="text" name="password" placeholder="password" />
+    </form>
+    <button type="submit" form"log-in-form">click!</button>"
+  `
+}
+const main = `
+  ${element.header}
+  ${element.form}
+  ${element.body}
+  ${element.footer}
+`
