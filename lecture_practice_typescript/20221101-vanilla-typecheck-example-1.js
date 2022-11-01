5823272a5f22590e1cef4913c7809900e4baa130
seunghyun kim <kshyun.dev@gmail.com>
2022-11-01 14:03:21 +0900

    바닐라 자바스크립트 타입체크

---
+++ lecture_practice_typescript/20221101-vanilla-typecheck-example-1.js
@@ -0,0 +1,8 @@
+function original(a, b) {
+  if(typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else {
+    console.log("parameter is type error");
+  }
+}
+console.log(original("1", "2"));
