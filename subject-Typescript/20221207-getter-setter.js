4d4d872ec4e0dce3dc860605323ff1167bae9cf7
kshyun1223 <kshyun.dev@gmail.com>
2022-12-07 11:02:09 +0900

    getter-setter 예제 (타스에서는 왜 에러가 뜰까?)

---
+++ subject-Typescript/20221207-getter-setter.js
@@ -0,0 +1,25 @@
+class BasicStudentInfo {
+  constructor() {
+    this.studentName = studentName
+    this.phoneNumber = phoneNumber
+    this.subNumber = subNumber
+    this.email = email
+    this.cityName = cityName
+  }
+  get cityName(){
+    return this._cityName
+  }
+  set cityName(value) {
+    if(typeof value === "string") {
+      this._cityName = value
+    } else {
+      console.log("데이터타입이 string이 아닌 관계로 강제로 바꾸었습니다.")
+      this._cityName = String(value)
+    }
+  }
+}
+
+const kongukjae = new BasicStudentInfo("공욱재", "010-9139-0957", "042-476-2111", "kongukjae@gmail.com", 12345)
+
+
+export{}
