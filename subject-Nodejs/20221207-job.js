4a7ac67ffeca8dd8db72b3b5badcbdb1997a95cc
kshyun1223 <kshyun.dev@gmail.com>
2022-12-08 09:20:17 +0900

    객체 예제

---
+++ subject-Nodejs/20221207-job.js
@@ -0,0 +1,15 @@
+function company(object) {
+  // algorithm
+  return {
+    companyName : "",
+    mainStack: [],
+    subStack: [],
+    businessModel: [] || "",
+    positiveFeather: [],
+    negativeFeather: []
+  }
+}
+
+let hoobo = [];
+
+company("asdf")
